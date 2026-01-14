6ffb9714f1ecb541a514a6aad8ff73fa462b8cb2
diyarfaraj <diyar.faraj@gmail.com>
2026-01-14 20:39:50 +0100

    fix email

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -74,10 +74,10 @@ export default function Footer() {
             className="pt-12 border-t border-white/10"
           >
             <a
-              href="mailto:hello@ame-studio.com"
+              href="mailto:info@ame-studio.com"
               className="text-gray-300 hover:text-white transition-colors duration-300"
             >
-              hello@ame-studio.com
+              info@ame-studio.com
             </a>
           </motion.div>
 
